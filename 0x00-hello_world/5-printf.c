97eeb695000644c31d52a1c0932ad62c103353cd
SpecialDude <warithadetayo.awa@gmail.com>
2022-06-02 17:13:45 -0700

    First C source

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Print text using the printf function
+ *
+ * Return: 0 if all statements are executed successfully
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
