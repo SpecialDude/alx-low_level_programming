5ef38c42dd729ac14d1f5d1c52380e3c0f7a8b6b
SpecialDude <warithadetayo.awa@gmail.com>
2022-06-02 17:05:54 -0700

    First C source

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
-/* Printing some text using the puts function */
+/**
+ * Printing some text using the puts function
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
