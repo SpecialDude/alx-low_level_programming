372d9a69676d5a1f13cfed600033dca93043727e
SpecialDude <warithadetayo.awa@gmail.com>
2022-06-02 17:02:38 -0700

    First C source

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 /* Printing some text using the puts function */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
