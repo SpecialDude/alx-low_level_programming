9c408fc889071764dc28bc6e714f8cca10bc57bc
SpecialDude <warithadetayo.awa@gmail.com>
2022-06-03 19:57:57 -0700

    Task 7 - Print alphabet in reverse

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -3,12 +3,13 @@
 /**
  * main - Print alphabet in revers
  *
+ * Return: 0 Success
  */
 int main(void)
 {
 	char c;
 
-	for (c = 'z'; c <= 'a'; c--)
+	for (c = 'z'; c >= 'a'; c--)
 		putchar(c);
 	putchar('\n');
 	return (0);
