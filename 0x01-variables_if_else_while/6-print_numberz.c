8d524f781e7ccbf57ac677b3e47adddbfdeb5292
SpecialDude <warithadetayo.awa@gmail.com>
2022-06-03 19:47:30 -0700

    Task 6 - Print Numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+
+/**
+ * main - Print Numberrs
+ *
+ * Return: 0 Success
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		putchar((char)n);
+	putchar('\n');
+
+	return (0);
+}
