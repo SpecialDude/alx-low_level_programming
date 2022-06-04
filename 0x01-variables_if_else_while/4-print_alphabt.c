e8381355c8cd24f7cbee59563442b0937625b067
SpecialDude <warithadetayo.awa@gmail.com>
2022-06-03 19:36:39 -0700

    Task 4 - Print Alphabet Lowercase omitting q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Printing alphabets omitting q and e
+ *
+ * Return: 0 Success
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c < 'z'; c++)
+	{
+		if (c != 'e' && c!= 'q')
+			putchar(c);
+	}
+	putchar('\n');
+
+	return (c);
+}
