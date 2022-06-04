25efe5b2f79b96c7866918ed903a8ea93507a2ff
SpecialDude <warithadetayo.awa@gmail.com>
2022-06-03 20:15:46 -0700

    Task 9 - Combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Combination
+ *
+ * Return: 0 Success
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 57; n++)
+	{
+		putchar(n);
+		putchar(', ')
+	}
+	putchar(57);
+
+	return (0);
+}
+
