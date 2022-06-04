2cccda6cb12e90ebc31141643a5b656c9916064d
SpecialDude <warithadetayo.awa@gmail.com>
2022-06-03 20:04:12 -0700

    Task 8 - Print hex

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+                                                                                                                                       
+/**
+ * main - Print alphabet in revers
+ *
+ * Return: 0 Success
+ */
+int main(void)
+{
+	char c;
+	int n;
+
+	for (n = 48; n < 58; n++)
+		putchar(n);
+
+	for (c = 'a'; c > 'g'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
