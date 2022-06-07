725dabcd616a8840e4a85f4a42730d5ce8a49623
SpecialDude <warithadetayo.awa@gmail.com>
2022-06-07 15:32:33 -0700

    Task 8 - Functions in C

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Print Every minute of the day
+ *
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int i;
+	for (i = 0; i < 24; i++)
+	{
+		int j;
+		for (j = 0; j < 60; j++)
+		{
+			if (i < 10)
+				_putchar('0');
+			_putchar(i + '0');
+			_putchar(':');
+			if (j < 10)
+				_putchar('0');
+			_putchar(j + '0');
+		}
+		_putchar('\n');
+	}
+}
