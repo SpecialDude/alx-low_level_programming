b34186762544cf03679cf22e890ada707fc72983
SpecialDude <warithadetayo.awa@gmail.com>
2022-06-07 16:30:11 -0700

    Task 8 - Functions in C

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - add two numbers together
+ *
+ * @n: First number
+ * @m: Second number
+ *
+ * Return: Sum of the two numbers (int)
+int add(int n, int m)
+{
+	return (n + m);
+}
