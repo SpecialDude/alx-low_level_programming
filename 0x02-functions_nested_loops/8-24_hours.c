099b4c4c10dc942fd3b25c1922d66986a2bc319a
SpecialDude <warithadetayo.awa@gmail.com>
2022-06-07 15:51:33 -0700

    Task 8 - Functions in C

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,25 +8,21 @@
 void jack_bauer(void)
 {
 	int i;
+
 	for (i = 0; i < 24; i++)
 	{
 		int j;
+
 		for (j = 0; j < 60; j++)
 		{
-			if (i < 10)
-				_putchar('0');
-			else
-				_putchar((i / 10) + '0');
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
 
-			_putchar((i % 10));
 			_putchar(':');
 
-			if (j < 10)
-				_putchar('0');
-			else
-				_putchar((j / 10) + '0');
-
+			_putchar((j / 10) + '0');
 			_putchar((j % 10) + '0');
+
 			_putchar('\n');
 		}
 	}
