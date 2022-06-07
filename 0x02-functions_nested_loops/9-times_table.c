edf6e666a2fc0f20f52c2f5ced3898e0e99d2974
SpecialDude <warithadetayo.awa@gmail.com>
2022-06-07 16:04:23 -0700

    Task 8 - Functions in C

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * times_table - Print multiplication table 9
+ *
+ * Return: void
+ */
+void time_table(void)
+{
+	int i = 0;
+
+	for (; i < 10; i++)
+	{
+		int j = 0;
+
+		for (; j < 10; j++)
+		{
+			int m = i * j;
+
+			_putchar(m + '0');
+
+			if (j < 9)
+				_putchar(',');
+
+				_putchar(' ');
+
+				if (m < 10)
+				{
+					_putchar(' ');
+				}
+		}
+		_putchar('$');
+		_putchar('\n');
+	}
+}
