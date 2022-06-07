9fb5b99d21fe434de2c71b96074d8bca6ec9390a
SpecialDude <warithadetayo.awa@gmail.com>
2022-06-07 13:23:31 -0700

    Task 0 - Functions in C

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Print Test to the console
+ *
+ * Return - 0 success
+ */
+int main(void)
+{
+	const char* text = "_putchar";
+
+	for (int i = 0; i < 8; i++)
+	{
+		putchar(text[i]
+);
+	}
+	putchar('\n');
+
+	return (0);
+}
