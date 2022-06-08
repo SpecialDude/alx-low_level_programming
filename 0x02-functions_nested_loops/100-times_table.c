28231e7052a3159abfca3c795ea566fe6631805c
SpecialDude <warithadetayo.awa@gmail.com>
2022-06-07 17:51:39 -0700

    Task 12 - Functions in C

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,8 +13,6 @@ void print_times_table(int n)
 
 	if (n < 0 || n > 15)
 		return;
-
-
 	for (i = 0; i <= n; i++)
 	{
 		int j;
@@ -37,14 +35,13 @@ void print_times_table(int n)
 				_putchar(' ');
 				_putchar(m + '0');
 			}
-			else if (m >=10 && m < 100)
+			else if (m >= 10 && m < 100)
 			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
 				_putchar((m / 10) + '0');
 				_putchar((m % 10) + '0');
-
 			}
 			else if (m >= 100)
 			{
@@ -53,7 +50,6 @@ void print_times_table(int n)
 				_putchar((m / 100) + '0');
 				_putchar(((m / 10) % 10) + '0');
 				_putchar((m % 10) + '0');
-
 			}
 		}
 		_putchar('\n');
