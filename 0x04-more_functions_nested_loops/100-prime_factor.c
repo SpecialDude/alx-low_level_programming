dfb2ca645b740a56770dc1118020bc14a0ebb23b
SpecialDude <warithadetayo.awa@gmail.com>
2022-06-09 16:34:23 -0700

    Task 11 - More C Functiosn

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Find the largest Prime factor of a number
+ *
+ * Return: 0 Success
+ */
+int main(void)
+{
+	long int n = 612852475143;
+	long int divider = 2;
+	long int max_factor;
+
+	while (n != 0)
+	{
+		if (n % divider != 0)
+		{
+			divider += 1;
+		}
+		else
+		{
+			max_factor = n;
+			n = n / divider;
+
+			if (n == 1)
+			{
+				break;
+			}
+		}
+	}
+	printf("%ld", max_factor);
+}
