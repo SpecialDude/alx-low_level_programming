9598c691505b02a99e31fff15b0f3bd74634e486
SpecialDude <warithadetayo.awa@gmail.com>
2022-06-09 14:17:27 -0700

    Task 0 - More C functions

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.c"
+
+/**
+ * _isupper - Check if a char is uppercase
+ *
+ * @c: Character
+ *
+ * Return: 1 if True 0 otherwis
+ */
+int _isupper(int c)
+{
+	return (c > 64 && c < 91);
+}
