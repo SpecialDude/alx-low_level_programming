74dbea43482063c16e4ca979c02754b4dc3686c6
SpecialDude <warithadetayo.awa@gmail.com>
2022-06-09 14:46:29 -0700

    Task 4 - More C Functions

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Print numbers without 2 and 4
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	char i = '0';
+
+	for (; i <= '9'; i++)
+	{
+		if (!(i == 2 || i == 4))
+			_putchar(i);
+	}
+	_putchar('\n');
+}
