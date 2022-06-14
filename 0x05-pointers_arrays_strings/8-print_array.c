731fa80ca1646eae3ec15c7fc3365e244abec5c8
SpecialDude <warithadetayo.awa@gmail.com>
2022-06-13 21:05:32 -0700

    Task 8 - Arrays and Pointers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,7 +12,12 @@ void print_array(int *a, int n)
 {
 	int i = 0;
 
-	for (; i < n; i++)
+	for (; i < n - 1; i++)
+	{
 		_putchar(a[i] + '0');
+		_putchar(',');
+		_putchar(' ');
+	}
+	_putchar(a[n - 1] + '0');
 	_putchar('\n');
 }
