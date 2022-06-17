4b924a656c7bba1663474a5ab82121bfe2bde03a
SpecialDude <warithadetayo.awa@gmail.com>
2022-06-16 19:30:00 -0700

    Task 2 - Pointer

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include "string.h"
+
+/**
+ * _strncpy - Copy Strings
+ *
+ * @dest: Destination str
+ * @src: Source str
+ * @n: number
+ *
+ * Return: Pointer to dest str
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	dest[i] = '\0'
+
+	return (dest);
+}
