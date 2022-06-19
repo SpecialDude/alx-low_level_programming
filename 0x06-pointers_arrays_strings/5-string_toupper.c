3f959f5491a571d7c5d64d9f58f81399f2a8dce8
SpecialDude <warithadetayo.awa@gmail.com>
2022-06-19 04:53:12 +0100

    Task 4 - Reverse Array

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * string_toupper - Change lowercases to uppercases
+ * 
+ * @char: String
+ * 
+ * Return: Pointer to string
+ */
+char *string_toupper(char *c)
+{
+    int i = 0;
+
+    while (*(c + i) != '\0')
+    {
+        if (*(c + i) >= 'a' && *(c + i) <= 'z')
+        {
+            *(c + i) = (char)((int)*(c + i) - 32);
+        }
+    }
+
+    return (c);
+}
