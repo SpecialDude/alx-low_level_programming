9dcad20697195a1bb86da3fbdc93fa59a915fc41
SpecialDude <warithadetayo.awa@gmail.com>
2022-06-19 05:42:02 +0100

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,6 +1,25 @@
 #include "main.h"
 
 /**
+ * char_in - Character in a String
+ *
+ * @str: string
+ * @c: Char
+ * @return int 
+ */
+int char_in(char *str, char c)
+{
+	int j = 0;
+
+	for (; str[j] != '\0'; j++)
+	{
+		if (str[j] == c)
+			return (1);
+	}
+
+	return (0);
+}
+/**
  * cap_string - Capitalize Each word
  * 
  * @c: String
@@ -11,6 +30,9 @@ char *cap_string(char *c)
 {
 	int i = 0;
 
+	char seps[13] = {' ', '\t', '\n', ',', ';', '.',
+		'!', '?', '"', '(', ')', '{', '}'};
+
 	if (*c >= 'a' && *c <= 'z')
 	{
 		*c = (char)((int)*c - 32);
@@ -18,7 +40,7 @@ char *cap_string(char *c)
 
 	while (*(c + i) != '\0')
 	{
-		if (*(c + i) == ' ')
+		if (char_in(seps, *(c + i)))
 		{
 			if (*(c + i + 1) >= 'a' && *(c + i + 1) <= 'z')
 			{
