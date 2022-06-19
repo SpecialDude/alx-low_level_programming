bbb780ec3a3cdbf55a44e24dc68e5b80f3f07bfa
SpecialDude <warithadetayo.awa@gmail.com>
2022-06-19 06:24:28 +0100

    Task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rot13 - Encode string with ROT13
+ *
+ * @c: String
+ *
+ * Return: char*
+ */
+char *rot13(char *c)
+{
+    int i, j;
+
+    char *alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    char *alph4 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+    for (i = 0; c[i] != "\0"; i++)
+    {
+        for (j = 0; alpha[j] != '\0'; j++)
+        {
+            if (c[i] == alpha[j])
+                c[i] = alph4[j];
+        }
+    }
+
+    return (c);
+}
