a411cc6c8530c6647bf24bccfea0fda859b571a4
SpecialDude <warithadetayo.awa@gmail.com>
2022-06-19 06:25:38 +0100

    Task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,19 +9,19 @@
  */
 char *rot13(char *c)
 {
-    int i, j;
+	int i, j;
 
-    char *alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    char *alph4 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char *alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *alph4 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-    for (i = 0; c[i] != "\0"; i++)
-    {
-        for (j = 0; alpha[j] != '\0'; j++)
-        {
-            if (c[i] == alpha[j])
-                c[i] = alph4[j];
-        }
-    }
+	for (i = 0; c[i] != "\0"; i++)
+	{
+		for (j = 0; alpha[j] != '\0'; j++)
+		{
+			if (c[i] == alpha[j])
+				c[i] = alph4[j];
+		}
+	}
 
-    return (c);
+	return (c);
 }
