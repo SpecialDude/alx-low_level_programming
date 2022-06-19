f91e8d7c81d730054f56f5f84931d26b4e1ba9d2
SpecialDude <warithadetayo.awa@gmail.com>
2022-06-19 06:05:10 +0100

    Task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+char *leet(char *c)
+{
+	int i, j;
+
+	char *wmap = "aAeEoOtTlL";
+	char *cmap = "4433007711";
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		for (j = 0; wmap[j] != '\0'; j++)
+		{
+			if (c[i] == wmap[j])
+				c[i] = cmap[j];
+		}
+	}
+
+	return (c);
+}
