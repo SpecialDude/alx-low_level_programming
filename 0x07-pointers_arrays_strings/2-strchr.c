1da0708e8c16630ee6e59e5bb0cbd773a93c459a
SpecialDude <warithadetayo.awa@gmail.com>
2022-06-21 04:05:22 +0100

    Task 9: Pointers and Strings

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -27,23 +27,15 @@ char *_strchr(char *s, char c)
 	}
 }
 
-/**
- * commentedCode - A block of commented code
- * 
- * @s: String
- * @c: Char
- * Return: char* 
- *
- * char *_strchr(char *s, char c)
- * {
- *	while ((*s) != '\0')
- *	{
- *		if ((*s) == c)
- *			return (s);
- *
- *		s++;
- *	}
- *
- * return (NULL);
- * }
- */
+// char *_strchr(char *s, char c)
+// {
+// 	while ((*s) != '\0')
+// 	{
+// 		if ((*s) == c)
+// 			return (s);
+
+// 		s++;
+// 	}
+
+// 	return (NULL);
+// }
