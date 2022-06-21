e0e953face2a4510a150c016db0f6f64d16c2632
SpecialDude <warithadetayo.awa@gmail.com>
2022-06-21 02:28:43 +0100

    Task 5: Pointers and Strings

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -19,7 +19,7 @@ char *_strstr(char *haystack, char *needle)
         for (; needle[j] != '\0'; j++)
         {
             if (haystack[i + j] != needle[j]);
-                break
+                break;
         }
 
         if (!needle[j])
