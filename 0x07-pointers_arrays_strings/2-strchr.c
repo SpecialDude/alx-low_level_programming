cbad98d0de7f0436e6dfdca6e8086b82a74f4530
SpecialDude <warithadetayo.awa@gmail.com>
2022-06-21 01:46:03 +0100

    Task 0 - 3: Pointers and Strings

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,12 +11,12 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
-	{
-		if (*s == c)
-			return (s);
+	int i = 0;
 
-		s++;
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) == c)
+			return (s + i);
 	}
 
 	return (NULL);
