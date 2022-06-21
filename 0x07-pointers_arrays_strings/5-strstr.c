2f7b253c7a356d36c26d13cdf0e7c9e30338f401
SpecialDude <warithadetayo.awa@gmail.com>
2022-06-21 02:26:38 +0100

    Task 5: Pointers and Strings

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strstr - locates a substring
+ *
+ * @haystack: Parent String
+ * @needle: Substr
+ *
+ * Return: Pointer to the found substr or NULL if not found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+    int i = 0;
+    int j = 0;
+
+    for (; haystack[i] != '\0'; i++)
+    {
+        for (; needle[j] != '\0'; j++)
+        {
+            if (haystack[i + j] != needle[j]);
+        }
+
+        if (!needle[j])
+            return (&haystack[i]);
+    }
+
+    return (NULL);
+}
