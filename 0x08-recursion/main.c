c327f7ab0b5e5be7457d19f0b7d49859faca4931
SpecialDude <warithadetayo.awa@gmail.com>
2022-06-23 00:51:12 +0100

    Recursion

---
+++ 0x08-recursion/main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+int main()
+{
+    char *s = "Tayo";
+    char *m = "Adeoti";
+
+    _puts_recursion(s);
+    _puts_recursion(m);
+
+    _print_rev_recursion(m);
+    _print_rev_recursion(s);
+}
