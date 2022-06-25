1b75d98ad88e4f8707806562ec1659accd05b336
SpecialDude <warithadetayo.awa@gmail.com>
2022-06-25 03:51:37 +0100

    Command Line Args

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,13 +13,13 @@
 int main(int argc, char *argv[])
 {
 	int i, sum, j;
+
 	int charOnly = 1;
 	sum = 0;
 
 	for (j = 0; j < argc; j++)
 	{
 		i = atoi(argv[j]);
-
 		if (!(i == 0 && !strcmp(argv[j], "0")))
 		{
 			charOnly = 0;
@@ -36,18 +36,14 @@ int main(int argc, char *argv[])
 	for (j = 0; j < argc; j++)
 	{
 		i = atoi(argv[j]);
-
 		if (i == 0 && !strcmp(argv[j], "0"))
 		{
 			printf("Error\n");
 
 			return (1);
 		}
-
 		sum += i;
 	}
-
 	printf("%d\n", sum);
-
 	return (EXIT_SUCCESS);
 }
