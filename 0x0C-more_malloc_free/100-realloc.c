2dd5478b28afea7b92dde804494ad33f87655bf5
SpecialDude <warithadetayo.awa@gmail.com>
2022-07-01 00:42:46 +0100

    More malloc_free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -39,7 +39,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (newptr != NULL)
 	{
-		if (new_size != old_size && new_size != 0)
+		if (new_size != 0)
 		{
 			memcpy(newptr, ptr, _min(old_size, new_size));
 		}
