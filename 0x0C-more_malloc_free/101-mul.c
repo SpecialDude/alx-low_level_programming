be3671b543052e9fe930ab553321bac22efbc677
SpecialDude <warithadetayo.awa@gmail.com>
2022-07-01 02:42:16 +0100

    More malloc_free

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,99 +1,91 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "main.h"
+#include <unistd.h>
 
 /**
- * is_digit - checks if a string contains a non-digit char
- * @s: string
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * Return: 0 if a non-digit is found, 1 otherwise
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-int is_digit(char *s)
+int _putchar(char c)
 {
-	int i = 0;
-
-	while (s[i])
-	{
-		if (s[i] < '0' || s[i] > '9')
-			return (0);
-		i++;
-	}
-	return (1);
+    return (write(1, &c, 1));
 }
 
 /**
- * _strlen - returns the length of a string
- * @s: string
+ * _putstr - Print String
  *
- * Return: int
+ * @str: The string
  */
-int _strlen(char *s)
+void _putstr(char *str)
 {
-	int i = 0;
+    int i = 0;
 
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
+    while (str[i])
+    {
+        _putchar(str[i]);
+        i++;
+    }
+
+}
+
+int isalldigit(char *str)
+{
+    int i = 0;
+
+    while (str[i])
+    {
+        if (str[i] < '0' || str[i] > '9')
+            return (0);
+        i++;
+    }
+    return (1);
 }
 
 /**
- * errors - handles errors for main
+ * print_number - prints an integer
+ * @n: integer to be printed
  */
-void errors(void)
+void print_number(int n)
 {
-	printf("Error\n");
-	exit(98);
+	unsigned int n1;
+
+	if (n < 0)
+	{
+		n1 = -n;
+		_putchar('-');
+	}
+	else
+	{
+		n1 = n;
+	}
+
+	if (n1 / 10)
+	{
+		print_number(n1 / 10);
+	}
+
+	_putchar((n1 % 10) + '0');
 }
 
-/**
- * main - multiplies two positive numbers
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: always 0 (Success)
- */
-int main(int argc, char *argv[])
+int main(int ac, char *av[])
 {
-	char *s1, *s2;
-	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+    if (ac != 3 )
+    {
+        _putstr("Error\n");
+        exit(98);
+    }
 
-	s1 = argv[1], s2 = argv[2];
-	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
-		errors();
-	len1 = _strlen(s1);
-	len2 = _strlen(s2);
-	len = len1 + len2 + 1;
-	result = malloc(sizeof(int) * len);
-	if (!result)
-		return (1);
-	for (i = 0; i <= len1 + len2; i++)
-		result[i] = 0;
-	for (len1 = len1 - 1; len1 >= 0; len1--)
-	{
-		digit1 = s1[len1] - '0';
-		carry = 0;
-		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
-		{
-			digit2 = s2[len2] - '0';
-			carry += result[len1 + len2 + 1] + (digit1 * digit2);
-			result[len1 + len2 + 1] = carry % 10;
-			carry /= 10;
-		}
-		if (carry > 0)
-			result[len1 + len2 + 1] += carry;
-	}
-	for (i = 0; i < len - 1; i++)
-	{
-		if (result[i])
-			a = 1;
-		if (a)
-			_putchar(result[i] + '0');
-	}
-	if (!a)
-		_putchar('0');
-	_putchar('\n');
-	free(result);
-	return (0);
+    if (!(isalldigit(av[1]) && isalldigit(av[2])))
+    {
+        _putstr("Error\n Not digits");
+        exit(98);
+    }
+
+    print_number(atoi(av[1]) * atoi(av[2]));
+    _putchar('\n');
+
+    return (EXIT_SUCCESS);
 }
