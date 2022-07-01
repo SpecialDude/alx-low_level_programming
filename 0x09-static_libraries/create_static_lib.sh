c0109d96bae9ee56f4731baeefeec6f68920b1d1
SpecialDude <warithadetayo.awa@gmail.com>
2022-07-01 01:00:59 +0100

    More malloc_free

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -5,7 +5,7 @@ do
     name=$(echo $file | rev | cut  -d / -f 1 | rev )
     filename=$(echo $name | cut  -d . -f 1)
 
-    echo "Building $file --> $./filename.o"
+    # echo "Building $file --> $./filename.o"
     gcc -g -O -c $file
 
     if [ $? -eq 0 ];
@@ -13,18 +13,18 @@ do
         echo "Archiving $./filename.o in liball.a"
         ar rc liball.a "$filename.o"
 
-        if [ $? -eq 0 ];
-        then
-            echo "Archiving done!!!"
-            rm -v ./"$filename.o"
-        else
-            echo "Archiving error" 
-        fi
-    else
-        echo "Build error"
+        # if [ $? -eq 0 ];
+        # then
+        #     echo "Archiving done!!!"
+        #     rm -v ./"$filename.o"
+        # else
+        #     echo "Archiving error"
+        # fi
+    # else
+    #     echo "Build error"
     fi
 
-    echo -e "\n"
+    # echo -e "\n"
 done
 
 ranlib ./liball.a
