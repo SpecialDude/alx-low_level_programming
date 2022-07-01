dd2130871cf34d5929c8b792b3571663cd7d8e7f
SpecialDude <warithadetayo.awa@gmail.com>
2022-07-01 02:25:16 +0100

    More malloc_free

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,5 +1,7 @@
+#include "main.h"
 #include <stdlib.h>
 #include <unistd.h>
+
 
 /**
  * _putchar - writes the character c to stdout
@@ -87,20 +89,53 @@ void print_number(int n)
  */
 int main(int ac, char *av[])
 {
-	if (ac != 3)
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+
+	if (ac != 3 || !(isalldigit(av[1]) && isalldigit(av[2])))
 	{
 		_putstr("Error\n");
 		exit(98);
 	}
 
-	if (!(isalldigit(av[1]) && isalldigit(av[2])))
-	{
-		_putstr("Error\n");
-		exit(98);
-	}
+	s1 = av[1], s2 = av[2];
 
 	print_number(atoi(av[1]) * atoi(av[2]));
 	_putchar('\n');
+
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
+	{
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		{
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
+		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
+	}
+	for (i = 0; i < len - 1; i++)
+	{
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
+	}
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
+	free(result);
 
 	return (EXIT_SUCCESS);
 }
