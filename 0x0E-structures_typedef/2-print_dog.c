12035b9525f2ba9d01ce9be5e7c4197aa4c27e6f
SpecialDude <warithadetayo.awa@gmail.com>
2022-07-05 18:26:29 +0100

    Preprocessor and Typedef

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,11 +10,11 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		printf("Name: %s\n", d->name == NULL ? "nill" : d->name);
+		printf("Name: %s\n", d->name == NULL ? "(nill)" : d->name);
 		if (&(d->age) == NULL)
-			printf("Age: %s\n", "nill");
+			printf("Age: %s\n", "(nill)");
 		else
 			printf("Age: %f\n", d->age);
-		printf("Owner: %s", d->owner == NULL ? "nill" : d->owner);
+		printf("Owner: %s", d->owner == NULL ? "(nill)" : d->owner);
 	}
 }
