9f59845e4beb0c604b13cbf5af1faa376a13ec0f
SpecialDude <warithadetayo.awa@gmail.com>
2022-07-05 18:52:29 +0100

    Preprocessor and Typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * new_dog - Create a new dog instance
@@ -12,14 +13,39 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *brand_new_dog = malloc(sizeof(dog_t));
+	dog_t *new_d;
+	int l1, l2;
 
-	if (brand_new_dog == NULL)
-		return (NULL);
+	new_d = malloc(sizeof(dog_t))
+	{
+		if (new_d == NULL)
+			return NULL;
+	}
 
-	brand_new_dog->name = name;
-	brand_new_dog->age = age;
-	brand_new_dog->owner = owner;
+	l1 = strlen(name);
 
-	return (brand_new_dog);
+	new_d->name = malloc(sizeof(char) * (l1 + 1));
+
+	if (new_d->name == NULL)
+	{
+		free(d);
+		return NULL;
+	}
+
+	l2 = strlen(name);
+
+	new_d->owner = malloc(sizeof(char) * (l2 + 1));
+
+	if (new_d->owner == NULL)
+	{
+		free(new_d->name)
+		free(new_d)
+		return NULL;
+	}
+
+	new_d->name = name;
+	new_d->age = age;
+	new_d->owner = owner;
+
+	return (new_d);
 }
