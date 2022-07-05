6cdfdb0cc3e44319567ac867e748712675bbbda2
SpecialDude <warithadetayo.awa@gmail.com>
2022-07-05 18:21:39 +0100

    Preprocessor and Typedef

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
  * init_dog - Initialize the member data of a dog struct
@@ -10,6 +11,14 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+	{
+		d = malloc(sizeof(struct dog));
+
+		if (d == NULL)
+			return (NULL);
+	}
+
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
