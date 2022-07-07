ba5ebb54b29f40c09d3508701780b14d6cdc2196
SpecialDude <warithadetayo.awa@gmail.com>
2022-07-07 01:52:10 +0100

    Function Pointers

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,17 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int av, char *av[])
+int main(int ac, char *av[])
 {
-	int byte;
+	int byte, i;
+	char *str;
 
-	if (av != 1)
+	if (ac != 2)
 	{
 		printf("Error\n");
-		return (98);
+		return (1);
 	}
 
 	byte = atoi(av[1]);
+
+	if (byte < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+
+	str = (char *)main;
+
+	for (i = 0; i < byte - 1; i++)
+		printf("%02hhx\n", str[i]);
+
+	printf("%02hhx\n", str[byte - 1]);
 
 	return (0);
 }
