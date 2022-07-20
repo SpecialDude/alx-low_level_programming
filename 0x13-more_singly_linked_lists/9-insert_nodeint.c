5cc46a740b023129478d666580f9da91a2888ac2
SpecialDude <warithadetayo.awa@gmail.com>
2022-07-21 00:21:59 +0100

    More Linked List

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -40,7 +40,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = (*head)->next;
 	}
 
-	if (*head || idx == i + 1)
+	if (*head || i == idx - 1)
 	{
 		newnode->next = (*head)->next;
 		(*head)->next = newnode;
