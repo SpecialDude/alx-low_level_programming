2d4301bc46fc928fefbf60b40ea6b1a26e4e2493
SpecialDude <warithadetayo.awa@gmail.com>
2022-07-20 04:32:48 +0100

    More Linked List

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,7 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (!*head && idx == 0)
 	{
 		newnode->next = NULL;
-		head = newnode;
+		*head = newnode;
 		return (newnode);
 	}
 
