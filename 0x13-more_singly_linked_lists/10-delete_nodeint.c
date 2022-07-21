e3968128ce35cbe2bfaec8f2418225f33dc5bae6
SpecialDude <warithadetayo.awa@gmail.com>
2022-07-21 01:07:58 +0100

    More Linked List

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -26,7 +26,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		free(deleted);
 	}
 
-	while (ptr && (i < index - 1))
+	while (ptr && (i < index - 2))
 	{
 		ptr = ptr->next;
 		i++;
