dc76f91736bc864a3fc3a8785fa27ec675c4b2bd
SpecialDude <warithadetayo.awa@gmail.com>
2022-09-06 02:47:54 +0100

    Dynamic Library: Task 2

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,66 @@
+#include "operations.h"
+
+/**
+ * add - add two numbers together
+ *
+ * @n: First number
+ * @m: Second number
+ *
+ * Return: Sum of the two numbers (int)
+ */
+int add(int n, int m)
+{
+	return (n + m);
+}
+
+/**
+ * sub - sub two numbers together
+ *
+ * @n: First number
+ * @m: Second number
+ *
+ * Return: diff of the two numbers (int)
+ */
+int sub(int n, int m)
+{
+	return (n - m);
+}
+
+/**
+ * mul - multiply two numbers together
+ *
+ * @n: First number
+ * @m: Second number
+ *
+ * Return: product of the two numbers (int)
+ */
+int mul(int n, int m)
+{
+	return (n * m);
+}
+
+/**
+ * div - divide two numbers together
+ *
+ * @n: First number
+ * @m: Second number
+ *
+ * Return: division of the two numbers (int)
+ */
+int div(int n, int m)
+{
+	return (n / m);
+}
+
+/**
+ * mod - n mod m (remainder when n divides m)
+ *
+ * @n: First number
+ * @m: Second number
+ *
+ * Return: mod of the two numbers (int)
+ */
+int mod(int n, int m)
+{
+	return (n % m);
+}
