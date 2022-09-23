a92f6d450d73f59007f845c3d46744cbfd8c2187
SpecialDude <warithadetayo.awa@gmail.com>
2022-09-23 13:21:07 +0100

    Hash Tables: Task 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,53 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - Updates an hash table
+ *
+ * @ht: An Hash Table
+ * @key: string value (cannot be empty or NULL)
+ * @value: string value (value to key)
+ *
+ * Return: int (1 on success 0 otherwise)
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *node, *new_node;
+
+	if (!key)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	node = *(ht->array + index);
+
+	while (node && node->next)
+	{
+		if (node->key == key)
+		{
+			node->value = (char *)value;
+			return (1);
+		}
+
+		node = node->next;
+	}
+
+	new_node = malloc(sizeof(hash_node_t));
+
+	if (new_node == NULL)
+		return (0);
+
+	if (node)
+	{
+		new_node->key = (char *)key;
+		new_node->value = (char *)value;
+
+		new_node->next = node->next;
+		node->next = new_node;
+	}
+	else
+	{
+		*(ht->array + index) = new_node;
+	}
+
+	return (1);
+}
