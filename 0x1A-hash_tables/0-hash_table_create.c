7242d344d177512d437c59f75d2ff074de167e4a
SpecialDude <warithadetayo.awa@gmail.com>
2022-09-23 12:03:17 +0100

    Hash Table: Task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,12 +10,21 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
+	hash_node_t *array;
 
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
 
+	array = malloc(sizeof(hash_node_t) * size);
+	if (array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
+
 	table->size = size;
+	table->array = &array;
 
 	return (table);
 }
