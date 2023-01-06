7a86aff32fedbc7bcdc15f62004c454bf7c8f556
SpecialDude <warithadetayo.awa@gmail.com>
2023-01-06 18:44:56 +0100

    Task 7

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,57 @@
+#include "search_algos.h"
+#include <math.h>
+
+#define min(x, y) ((x < y) ? x : y)
+
+/**
+ * jump_search - Searches value from array using
+ * jump searching algorithm
+ *
+ * @array: The array to search from
+ * @size: Size of the array
+ * @value: value to search for within the array
+ *
+ * Return: index where value was found otherwise -1
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	size_t low, high;
+	int step;
+
+	step = sqrt(size);
+
+	low = 0;
+	high = 0;
+
+	while (array[high] < value)
+	{
+		printf("Value checked array[%lu] = [%d]\n", high, array[high]);
+
+		low = high;
+		high += step;
+
+		if ((high) >= size)
+			break;
+	}
+
+	printf("Value found between indexes [%lu] and [%lu]\n", low, high);
+
+	high = min(high, size);
+
+	while (array[low] < value)
+	{
+		printf("Value checked array[%lu] = [%d]\n", low, array[low]);
+		low++;
+
+		if (low == high)
+			return (-1);
+	}
+
+	if (array[low] == value)
+	{
+		printf("Value checked array[%lu] = [%d]\n", low, array[low]);
+		return (low);
+	}
+	else
+		return (-1);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -7,6 +7,6 @@
 int linear_search(int *array, size_t size, int value);
 void print_array(int *array, size_t low, size_t high);
 int binary_search(int *array, size_t size, int value);
-
+int jump_search(int *array, size_t size, int value);
 #endif
 
