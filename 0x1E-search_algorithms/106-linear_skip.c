93a857a2ac67dff006acdfb096d4b26cefdb1c2f
SpecialDude <warithadetayo.awa@gmail.com>
2023-01-08 04:09:12 +0100

    Last Task

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -10,8 +10,43 @@
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
+	skiplist_t *prev, *current;
+
 	if (list == NULL)
 		return (NULL);
 
-	return (list);
+	prev = list;
+	current = list->express;
+	while (current && current->n < value)
+	{
+		printf("Value checked at index [%lu] = [%d]\n", current->index, current->n);
+		prev = current;
+		current = current->express;
+	}
+
+	if (current == NULL)
+	{
+		current = prev->next;
+		while (current->next)
+			current = current->next;
+	}
+	else
+		printf("Value checked at index [%lu] = [%d]\n", current->index, current->n);
+	printf("Value found between indexes [%lu] and [%lu]\n",
+		prev->index, current->index);
+	while (prev && prev->n < value)
+	{
+		printf("Value checked at index [%lu] = [%d]\n", prev->index, prev->n);
+		prev = prev->next;
+	}
+	if (prev == NULL)
+		return (NULL);
+
+	if (prev->n == value)
+	{
+		printf("Value checked at index [%lu] = [%d]\n", prev->index, prev->n);
+		return (prev);
+	}
+	else
+		return (NULL);
 }
